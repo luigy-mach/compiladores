2384ec02754950c47a6a262340f7f84a11cfbf82
luigy-mach <luigy.mach.arc@gmail.com>
2016-09-20 12:59:27 -0500

    pre_processing completado

---
+++ automatas/automaton_preprocessing/test_pre_processing.cpp
@@ -0,0 +1,18 @@
+#include "pre_processing.hpp"
+
+#include <iostream>     // std::cout
+#include <fstream>      // std::ifstream
+
+
+#include<string>
+
+
+using namespace std;
+using namespace pre_processing;
+
+int main () {
+
+  pre_processing::pre_processing("test.txt","test_output.txt");
+
+  return 0;
+}
