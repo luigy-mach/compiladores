99aed14fd2289ae930407877c53709b9b5aaad6c
luigy-mach <luigy.mach.arc@gmail.com>
2016-12-30 07:48:12 -0500

    update

--- compilador_processing/processing/jhony4.ino
+++ compilador_processing/processing/jhony4.ino
@@ -1,14 +1,53 @@
-
- void loop() { 
-if (Serial.available ()>0){
-    estado = Serial.read();
-    if (estado > 1){
-      switch(estado){
-          case ' w ' :
-          adelante(vel); 
-          break;
-      }
-    }
-  }
+int pin3 =3;
+int pin5 =5;
+int pin6 =6;
+int pin9 =9;
+int estado;
+int vel = 75;
+void setup() {
+  Serial.begin (9600);
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
 }
+void activar(int v){
+    analogWrite(pin3, v); 
+    analogWrite(pin5, v);
+    analogWrite(pin6, v);
+    analogWrite(pin9, v);
+}
+void parar(){
+  analogWrite(pin3, 0);
+  analogWrite(pin5, 0);
+  analogWrite(pin6, 0);
+  analogWrite(pin9, 0);
+}
+void adelante(int v){
+    analogWrite(pin3, v); 
+    analogWrite(pin5, 0);
+    analogWrite(pin6, 0);
+    analogWrite(pin9, v);
+}
+void atras(int v){
+    analogWrite(pin3, 0); 
+    analogWrite(pin5, v);
+    analogWrite(pin6, v);
+    analogWrite(pin9, 0);
+  }
+void derecha(int v){
+    analogWrite(pin3, v); 
+    analogWrite(pin5, 0);
+    analogWrite(pin6, v);
+    analogWrite(pin9, 0);
+}
+
+
+void izquierda(int v){
+    analogWrite(pin3, 0); 
+    analogWrite(pin5, v);
+    analogWrite(pin6, 0);
+    analogWrite(pin9, v);
+}
+
 
